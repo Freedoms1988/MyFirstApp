2de725109fe8ad7e4fa6061f7d082f511e1038cf
Freedoms1988 <zzt328697768@sina.com>
2013-06-27 13:10:30 +0800

    11

---
+++ MyFirstApp/AFNetWorkingViewController.h
@@ -0,0 +1,15 @@
+//
+//  AFNetWorkingViewController.h
+//  MyFirstApp
+//
+//  Created by Freedoms on 13-6-26.
+//  Copyright (c) 2013年 周 忠亭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFNetWorking.h"
+#import "MBProgressHUD.h"
+#import "JSONKit/JSONKit.h"
+@interface AFNetWorkingViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
+
+@end
