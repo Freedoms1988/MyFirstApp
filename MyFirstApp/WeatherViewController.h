318fed381218c8604fb864c391f9dc0c0987fd0d
Freedoms1988 <zzt328697768@sina.com>
2013-06-28 11:13:58 +0800

    1

---
+++ MyFirstApp/WeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.h
+//  MyFirstApp
+//
+//  Created by Freedoms on 13-6-28.
+//  Copyright (c) 2013年 Freedoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherViewController : UIViewController
+
+@end
---
+++ MyFirstApp/WeatherViewController.m
@@ -0,0 +1,31 @@
+//
+//  WeatherViewController.m
+//  MyFirstApp
+//
+//  Created by Freedoms on 13-6-28.
+//  Copyright (c) 2013年 Freedoms. All rights reserved.
+//
+
+#import "WeatherViewController.h"
+#define CGRECT_
+@interface WeatherViewController ()
+{
+
+}
+@end
+
+@implementation WeatherViewController
+#pragma mark - ViewController Circle Life
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
